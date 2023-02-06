4f5d9de6e0a063fcbff10bb8c9f75cedbb4d35f5
06hulger01 <haciulger1980@gmail.com>
2023-02-06 18:33:00 +0300

    farklı

--- src/main/java/gitDeneme5/newFeature.java
+++ src/main/java/gitDeneme5/newFeature.java
@@ -4,5 +4,7 @@ public class newFeature {
     public static void main(String[] args) {
         System.out.println("new feature created");
         System.out.println("----------------");
+        System.out.println("farklı 2");
+
     }
 }
