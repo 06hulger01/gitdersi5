92ec6d156c594319b409e9f17e32f6c71b382425
06hulger01 <haciulger1980@gmail.com>
2023-02-06 17:58:51 +0300

    ikinci geliştirme yapıldı.

--- src/main/java/gitDeneme5/Test1.java
+++ src/main/java/gitDeneme5/Test1.java
@@ -3,6 +3,7 @@ package gitDeneme5;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("ilk geliştirme yapıldı");
+        System.out.println("---------------------");
     }
 
 }
