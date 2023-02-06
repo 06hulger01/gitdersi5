8b7ff1c4cb6614c0412ad6e1550d35e3d75c278c
06hulger01 <haciulger1980@gmail.com>
2023-02-06 18:15:02 +0300

    Added newFeature

--- src/main/java/gitDeneme5/Test1.java
+++ src/main/java/gitDeneme5/Test1.java
@@ -4,6 +4,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("ilk geliştirme yapıldı");
         System.out.println("---------------------");
+
+
     }
 
 }
---
+++ src/main/java/gitDeneme5/newFeature.java
@@ -0,0 +1,7 @@
+package gitDeneme5;
+
+public class newFeature {
+    public static void main(String[] args) {
+        System.out.println("new feature created");
+    }
+}
